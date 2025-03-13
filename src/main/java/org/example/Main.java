7e5fcc35a30dbc5981e524b56d002244b1a84976
TalkativeCat <dddd@ddd.com>
2025-03-13 18:02:22 +0500

    Решение тестового задания

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,38 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] array = {-58000, -2, 0, 51, 4839, 77, -22};
+        int[] result = testMetod(array);
+        System.out.println("Первое максимальное число: " + result[0]);
+        System.out.println("Второе максимальное число: " + result[1]);
+    }
+    // Проще возвращать в данном методе массив из чисел и далее проводить над ним операции в main
+    public static int[] testMetod(int[] array) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Проверим, что в массиве не меньше одного числа, иначе реализация метода не имеет смысла
+        if (array == null || array.length < 2) {
+            throw new IllegalArgumentException("Массив должен содержать минимум два числа");
         }
+
+        // Используем при инициализации переменных минимальные значения, допустимые для int
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        // Применим для перебора массива лаконичную конструкцию forEach
+        for (int number : array) {
+            // Если текущее число num больше, чем first, это означает, что мы нашли новое наибольшее число
+            if (number > first) {
+                // Старое наибольшее число из first перемещаем в second, firstMax обновлям значением из number
+                second = first;
+                first = number;
+            }
+            // Если текущее число number не больше first, но больше second, это означает, что мы нашли новое второе наибольшее число
+            else if (number > second) {
+                second = number;
+            }
+        }
+
+        // Возвращаем массив из двух чисел
+        return new int[]{first, second};
     }
 }
